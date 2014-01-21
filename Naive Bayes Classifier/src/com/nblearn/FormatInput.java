efdce549fec1125a10556a8755d333cd92989bfd
denis <denisjoseph333@gmail.com>
2014-01-20 18:44:47 -0800

    Extraction and formatting done

--- Naive Bayes Classifier/src/com/nblearn/FormatInput.java
+++ Naive Bayes Classifier/src/com/nblearn/FormatInput.java
@@ -9,47 +9,57 @@ import java.io.IOException;
 
 public class FormatInput {
 	
-	public static void createTrainingDataFile(){
+	public static void createTrainingDataFile() throws IOException{
 		BufferedReader br = null;
 		BufferedWriter bw = null;
-		try{
-			File file = new File("C:\\D Drive\\KNOWLEDGE IS POWER\\NLP\\HW1 Spam Filter and Sentiment Analysis\\SPAM_training\\spam_training.txt");
-	    	if( file.exists()){
-	    		file.delete();
-	    		file.createNewFile();
-	    	}
-				
-			
-			br = new BufferedReader(new FileReader("C:\\D Drive\\KNOWLEDGE IS POWER\\NLP\\HW1 Spam Filter and Sentiment Analysis\\SPAM_training\\HAM.3.txt"));
-			String curLine = null;
-			
-			// add a space delimeter for every new line except the first one
-			if( (curLine = br.readLine()) != null ){
-				bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile(), true)); //open file for appending
-		    	bw.write(curLine);	
-		    	bw.close();
-		    }		
-			while( (curLine = br.readLine()) != null ){
-		    	bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile(), true)); //open file for appending
-		    	bw.write(" " + curLine);
-		    	bw.close();
-		    }	
-		    
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		finally{
+		File dir = new File("C:\\D Drive\\KNOWLEDGE IS POWER\\NLP\\HW1 Spam Filter and Sentiment Analysis\\test");
+    	File spamTrainingFile = new File("C:\\D Drive\\KNOWLEDGE IS POWER\\NLP\\HW1 Spam Filter and Sentiment Analysis\\test\\spam_training.txt");
+		
+    	if( spamTrainingFile.exists()){
+    		spamTrainingFile.delete();
+    		spamTrainingFile.createNewFile();
+    	}
+		
+    	for(String fileName : dir.list()){    		
 			try{
-				if( br != null )
-					br.close();
-				if( bw != null )
-					bw.close();
+				
+				//check if the file being examined is the spam training file itself!!
+				if( (dir.getAbsolutePath() + "\\"+ fileName).equals(spamTrainingFile.getAbsolutePath()))
+				  continue;
+				
+				System.out.println("Extracting text from " + dir.getAbsolutePath() + "\\"+ fileName + "...");
+				
+				br = new BufferedReader(new FileReader( dir.getAbsolutePath() + "\\"+ fileName) );
+				bw = new BufferedWriter(new FileWriter(spamTrainingFile.getAbsoluteFile(), true)); //open file for appending
+				String curLine = null;
+				String label = "yo";
+				if( fileName.toUpperCase().contains("SPAM"))
+					label = "SPAM";
+				if( fileName.toUpperCase().contains("HAM"))
+					label = "HAM";
+				
+				bw.write(label + " ");
+				while( (curLine = br.readLine()) != null )		    	
+			    	bw.write(" " + curLine);
+	
+				bw.write("\n");
 			}
 			catch(IOException e){
 				e.printStackTrace();
-			}	
-		}
+			}
+			finally{
+				try{
+					if( br != null )
+						br.close();
+					if( bw != null )
+						bw.close();
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}	
+		    }
+		
+    	}
 	}
 	
 	
--- Naive Bayes Classifier/src/com/nblearn/Main.java
+++ Naive Bayes Classifier/src/com/nblearn/Main.java
@@ -1,5 +1,7 @@
 package com.nblearn;
 
+import java.io.IOException;
+
 public class Main {
 
 	/**
@@ -7,7 +9,12 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		FormatInput.createTrainingDataFile();
+		try {
+			FormatInput.createTrainingDataFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
